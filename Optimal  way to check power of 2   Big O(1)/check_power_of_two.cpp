adfebfc7f61917e9231c18ce7b6a1cd52ed19bb4
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-08-30 17:08:02 +0530

    Update check_power_of_two.cpp

--- Optimal  way to check power of 2   Big O(1)/check_power_of_two.cpp
+++ Optimal  way to check power of 2   Big O(1)/check_power_of_two.cpp
@@ -4,5 +4,5 @@ int main()
 	bool k;
 	x & --x == 0? k=yes : k=no; // A number is power of two if " & " operation between given number and it's previous number is zero
 	
-	// ++x/--x -> increase first then use    or     x++/x-- -> use first then increase; 
+	// ++x/--x -> modify first then use    or     x++/x-- -> use first then modify; 
 }
