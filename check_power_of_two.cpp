b11af1f2dbe140efb8638347e9abcfe31ee00bc6
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-08-30 17:04:35 +0530

    Add files via upload

---
+++ check_power_of_two.cpp
@@ -0,0 +1,8 @@
+int main()
+{
+	int x // that number who is power of 2? possiblity is to check
+	bool k;
+	x & --x == 0? k=yes : k=no; // A number is power of two if " & " operation between given number and it's previous number is zero
+	
+	// ++x/--x -> increase first then use    or     x++/x-- -> use first then increase; 
+}
